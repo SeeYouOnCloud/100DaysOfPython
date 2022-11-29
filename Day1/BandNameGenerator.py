07c4daecb9cefd8a750a426efb671a9a418bde20
amrutapardeshi <amrutapardeshi07@gmail.com>
2022-11-29 20:46:48 +0530

    Day 1 Submitted

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print("Welcome to the Band Name Generator")
+a = (input("What's the name of City you grew up in? "))
+b = (input("What's your Pet's name? "))
+print("Your Band Name could be " +a, "" +b)
---
+++ Day1/InputFunction.py
@@ -0,0 +1,4 @@
+#Write a program that prints the number of characters in a user's name
+
+i = (input("What is your name? "))
+print(len(i))
