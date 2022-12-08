c53efb27af7b7597f60beb1dde4feb57421e1b27
amrutapardeshi <amrutapardeshi07@gmail.com>
2022-12-08 18:28:05 +0530

    new code

--- Day2/LifeInWeeks.py
+++ Day2/LifeInWeeks.py
@@ -1,9 +1,9 @@
 #Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
 age = input("What is your current age? ")
 
-years = 90 - int(age)
-months = round(years * 12)
-weeks = round(years * 52)
-days = round(years * 365)
+years_remaining = 90 - int(age)
+months = round(years_remaining * 12)
+weeks = round(years_remaining * 52)
+days = round(years_remaining * 365)
 
 print(f"You have {days} days, {weeks} weeks, and {months} months left.")
