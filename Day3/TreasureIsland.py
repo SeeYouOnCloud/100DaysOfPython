c64270b9d0ca759cf61c67724c3a8bc539f569d1
amrutapardeshi <amrutapardeshi07@gmail.com>
2022-12-19 15:20:13 +0530

    added some lines

--- Day3/TreasureIsland.py
+++ Day3/TreasureIsland.py
@@ -1,6 +1,6 @@
 #https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 #https://ascii.co.uk/art
-'''
+print('''
 
 *******************************************************************************
           |                   |                  |                     |
@@ -22,7 +22,7 @@ ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
 ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/[TomekK]
 *******************************************************************************
-'''
+''')
 
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
@@ -40,7 +40,9 @@ if choice1 == "left":
         elif choice3 == "yellow":
             print("You found a treasure ..You Win!!!")    
         else:
-            print("Game Over!!!")      
+            print("Game Over!!!") 
+    else:
+        print("Attacked by a trout ..Game Over!!!")             
 
 else:
     print("Fall into a hole ..Game Over!!!")  
